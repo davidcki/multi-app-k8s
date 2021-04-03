166f9639d7e425f6308a42dbc912031158282882
David <devidcki@gmail.com>
2021-04-03 15:02:26 +0200

    Finishes Travis CI configuration and deployment script

---
+++ deploy.sh
@@ -0,0 +1,17 @@
+docker build -t davidcki/multi-client:latest -t davidcki/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t davidcki/multi-server:latest -f davidcki/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t davidcki/multi-worker:latest -f davidcki/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+
+docker push davidcki/multi-client:latest
+docker push davidcki/multi-server:latest
+docker push davidcki/multi-worker:latest
+
+docker push davidcki/multi-client:$SHA
+docker push davidcki/multi-server:$SHA
+docker push davidcki/multi-worker:$SHA
+
+kubectl apply -f ./k8s
+
+kubectl set image deployments/client-deployment server=davidcki/multi-client:$SHA
+kubectl set image deployments/server-deployment server=davidcki/multi-server:$SHA
+kubectl set image deployments/worker-deployment server=davidcki/multi-worker:$SHA
